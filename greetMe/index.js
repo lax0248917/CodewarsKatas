5081d6164c14902491980c7623eac7174fc4c7e8
lax0248917 <lax0248917@gmail.com>
2020-05-23 10:24:16 -0400

    :rocket: Kata Solution Commit

--- greetMe/index.js
+++ greetMe/index.js
@@ -6,7 +6,7 @@ var greet = function(name) {
   }else if (name === name.toLowerCase()) {
     return 'Hello ' + name.charAt(0).toUpperCase() + name.slice(1) + '!';
   }else {
-    return 'Hello ' + firstLetter + lowerName.toLowerCase() + '!';
+    return 'Hello ' + firstLetter.toUpperCase() + lowerName.toLowerCase() + '!';
   }
 };
 
