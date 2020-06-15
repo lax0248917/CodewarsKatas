073dd5967e884fff53da03adf80872c2af3358dd
lax0248917 <lax0248917@gmail.com>
2020-06-14 22:31:19 -0400

    :rocket:Kata Solution Commit

--- willYouMakeIt/index.js
+++ willYouMakeIt/index.js
@@ -1,7 +1,10 @@
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-  // TODO
+  if ((distanceToPump / mpg) <= fuelLeft) {
+    return true;
+  } else {
+    return false;
+  }
 };
-
 
 console.log(zeroFuel(50, 25, 2), true);
 console.log(zeroFuel(100, 50, 1), false);
