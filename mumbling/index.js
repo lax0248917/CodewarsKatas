aa03443b27d96a10ff9201296a677bfb85edf98e
Zachary King <lax0248917@gmail.com>
2020-07-26 21:28:32 -0400

    :rocket: Code Kata initial commit

---
+++ mumbling/index.js
@@ -0,0 +1,8 @@
+function accum(str) {
+	// iterate over string and get each letter.
+	// push information into an array of each letter
+	// take each item in the array and capitlize it
+	// take the index + 1 and replicate the letter that many times
+	// template literal to concat the string together
+	// return value
+}
